3ef64f3375181844fd615b9b443c00bb5341d618
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-10-22 16:05:19 +0100

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -18,7 +18,7 @@
         <div>
           <label for="email" class="block text-sm/6 font-medium text-gray-500">Name</label>
           <div class="mt-2">
-            <input v-model="name" type="email" name="name" id="email" autocomplete="name" required="" class="block w-full rounded-md bg-indigo-500/5 px-3 py-1.5 text-base text-gray-500 outline-1 -outline-offset-1 outline-indigo-500/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input v-model="name" type="name" name="name" id="name" autocomplete="name" required="" class="block w-full rounded-md bg-indigo-500/5 px-3 py-1.5 text-base text-gray-500 outline-1 -outline-offset-1 outline-indigo-500/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
           </div>
         </div>
         <div>
