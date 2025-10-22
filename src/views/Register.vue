7eb0512ab6e32886a710acea489e716be5754ac7
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-10-22 16:10:17 +0100

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,7 +10,7 @@
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-indigo-500">Register on FinTrack</h2>
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-indigo-500">Sign up on FinTrack</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
