92e57579041f04ac857a561caaad7564e9ea75cc
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-11-13 16:34:51 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -14,17 +14,17 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (response) => response,
-  // (error) => {
-  //   if (error.response && error.response.status === 401) {
-  //     localStorage.removeItem("token");
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
 
-  //     if (window.location.pathname !== "/login") {
-  //       window.location.href = "/login";
-  //     }
-  //   }
+      if (window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
+    }
 
-  //   return Promise.reject(error);
-  // }
+    return Promise.reject(error);
+  }
 );
 
 
